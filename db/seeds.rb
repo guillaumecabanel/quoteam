f0363bacbc1bf84e7e137ea00b50813ccb1cb84f
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-02-15 11:38:13 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+puts "Removing existing quotes..."
+Quote.destroy_all
+
+puts "Generating fake quotes..."
 10.times do
   quote = Quote.create!(
-    content: Faker::Commerce.product_name,
+    content: Faker::ChuckNorris.fact,
     author: Faker::Name.name,
   )
 end
