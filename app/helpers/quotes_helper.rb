1052e9e1384de5423fc76c718741bdba276bca0c
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-02-16 12:22:47 +0100

    fix file name help

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,4 +1,4 @@
-module QuoteHelper
+module QuotesHelper
   def quote_like_css(user, quote)
     return "fa fa-2x fa-heart-o" if user.nil?
     user.liked?(quote) ? "fa fa-2x fa-heart" : "fa fa-2x fa-heart-o"
