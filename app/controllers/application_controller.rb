fa083a572994ca602ed949f6a7fe6f6fb679a722
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-02-19 12:46:19 +0100

    fix extract_locale if no language is set.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < ActionController::Base
   def set_locale
     # Choosing an Implied Locale (http://guides.rubyonrails.org/i18n.html#choosing-an-implied-locale)
     I18n.locale = ( params[:locale] || extract_locale_from_accept_language_header ) || I18n.default_locale
-    raise
   end
 
   def default_url_options
