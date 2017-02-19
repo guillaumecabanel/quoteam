f9298802ce15ded16aa99906c553fc873bf2dec5
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-02-19 21:35:22 +0100

    improved front

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,6 +1,7 @@
 module QuotesHelper
   def quote_like_css(user, quote)
-    return "fa fa-2x fa-heart-o" if user.nil?
-    user.liked?(quote) ? "fa fa-2x fa-heart" : "fa fa-2x fa-heart-o"
+    icon = "fa fa-heart"
+    return "#{icon}-o" if user.nil?
+    user.liked?(quote) ? icon : "#{icon}-o"
   end
 end
