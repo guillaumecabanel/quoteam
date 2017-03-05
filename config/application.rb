3852ef373968e72bb8ad0fd1ca589e2c8c4ef8db
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-03-05 19:43:14 +0100

    ajax for like done

--- config/application.rb
+++ config/application.rb
@@ -18,15 +18,15 @@ Bundler.require(*Rails.groups)
 
 module Quoteam
   class Application < Rails::Application
-      config.generators do |generate|
-    generate.assets false
-    generate.helper false
-  end
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     config.i18n.default_locale = :en
   end
 end
